5e5a41c9d80b952b333cdcbd66d7e354a5a3d309
Curtis Chong <curtischong5@gmail.com>
2019-01-06 13:39:02 -0500

    server now posts to influx

--- bioworker/bio_worker.go
+++ bioworker/bio_worker.go
@@ -5,3 +5,27 @@ type BioSnapshot struct {
 	TimeEnd   string `json:"timeend"`
 	Heartrate string `json:"heartrate"`
 }
+
+type EmotionEvaluation struct {
+	TimeStartFillingForm string `json:"timeStartFillingForm"`
+	TimeEndFillingForm   string `json:"timeEndFillingForm"`
+	NormalEval           string `json:"normalEval"`
+	SocialEval           string `json:"socialEval"`
+	ExhaustedEval        string `json:"exhaustedEval"`
+	TiredEval            string `json:"tiredEval"`
+	HappyEval            string `json:"happyEval"`
+	Comments             string `json:"comments"`
+}
+
+type MarkEvent struct {
+	TimeStartFillingForm string `json:"timeStartFillingForm"`
+	TimeEndFillingForm   string `json:"timeEndFillingForm"`
+	TimeOfMark           string `json:"timeOfMark"`
+	IsReaction           string `json:"isReaction"`
+	LeftCrop             string `json:"leftCrop"`
+	RightCrop            string `json:"rightCrop"`
+	TimeOfEvent          string `json:"TimeOfEvent"`
+	EmotionsFelt         string `json:"happyEval"`
+	Comments             string `json:"comments"`
+	TypeBiometricsViewed string `json:"typeBiometricsViewed"`
+}
--- main/main.go
+++ main/main.go
@@ -6,23 +6,46 @@ import (
 	"fmt"
 	bioworker "github.com/curtischong/lizzie_server/bioworker"
 	typerworker "github.com/curtischong/lizzie_server/typerworker"
+	"github.com/influxdata/influxdb/client/v2"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
+type Config struct {
+	DatabaseConfig DatabaseConfigObj `json:"databaseconfig"`
+}
+type DatabaseConfigObj struct {
+	Dbname   string `json:"dbname"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 type server struct {
 	router *http.ServeMux
 }
 
+func stringToDate(unparsed string) time.Time {
+	layout := "2006-01-02 15:04:05"
+	t, err := time.Parse(layout, unparsed)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fmt.Println(t.Format("20060102150405"))
+	return t
+}
+
 // Watch
-func (s server) routes() {
+func (s server) routes(config DatabaseConfigObj) {
 	s.router.HandleFunc("/watch_bio_snapshot", s.watchBioSnapshotCall())
 	s.router.HandleFunc("/typer_sent_field", s.typerSentFieldCall())
-	s.router.HandleFunc("/upload_emotion_evaluation", s.uploadEmotionEvaluationCall())
+	s.router.HandleFunc("/upload_emotion_evaluation", s.uploadEmotionEvaluationCall(config))
+	s.router.HandleFunc("/upload_mark_event", s.uploadMarkEvent(config))
 	//s.router.HandleFunc("/admin", s.adminOnly(s.handleAdminIndex()))
 }
-func (s *server) uploadEmotionEvaluationCall() http.HandlerFunc {
+func (s *server) uploadEmotionEvaluationCall(config DatabaseConfigObj) http.HandlerFunc {
 	return func(w http.ResponseWriter, response *http.Request) {
 
 		if response.Body == nil {
@@ -48,6 +71,8 @@ func (s *server) uploadEmotionEvaluationCall() http.HandlerFunc {
 
 		fmt.Println(parsedResonse.TiredEval)
 		fmt.Fprintf(w, "bio snapshot")
+
+		insertEmotionEvaluation(parsedResonse, config)
 	}
 }
 func (s *server) watchBioSnapshotCall() http.HandlerFunc {
@@ -75,6 +100,109 @@ func (s *server) watchBioSnapshotCall() http.HandlerFunc {
 
 		fmt.Println(parsedResonse.Heartrate)
 		fmt.Fprintf(w, "bio snapshot")
+	}
+}
+
+func insertEmotionEvaluation(sample bioworker.EmotionEvaluation, config DatabaseConfigObj) {
+	// Create a new HTTPClient
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     "http://10.8.0.1:8086",
+		Username: config.Username,
+		Password: config.Password,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	// Create a new point batch
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  config.Dbname,
+		Precision: "s",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a point and add to batch
+	//tags := map[string]string{"": "cpu-total"}
+	fields := map[string]interface{}{
+		"timeStartFillingForm": sample.TimeStartFillingForm,
+		"normalEval":           sample.NormalEval,
+		"socialEval":           sample.SocialEval,
+		"exhaustedEval":        sample.ExhaustedEval,
+		"tiredEval":            sample.TiredEval,
+		"happyEval":            sample.HappyEval,
+		"comments":             sample.Comments,
+	}
+
+	pt, err := client.NewPoint("emotionEvaluation", nil, fields, stringToDate(sample.TimeEndFillingForm))
+	if err != nil {
+		log.Fatal(err)
+	}
+	bp.AddPoint(pt)
+
+	// Write the batch
+	if err := c.Write(bp); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("added point!")
+
+	// Close client resources
+	if err := c.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+//TODO: udpate the type to MarkEvent
+func uploadMarkEvent(sample bioworker.EmotionEvaluation, config DatabaseConfigObj) {
+	// Create a new HTTPClient
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     "http://10.8.0.1:8086",
+		Username: config.Username,
+		Password: config.Password,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	// Create a new point batch
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  config.Dbname,
+		Precision: "s",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a point and add to batch
+	//tags := map[string]string{"": "cpu-total"}
+	fields := map[string]interface{}{
+		"timeStartFillingForm": sample.TimeStartFillingForm,
+		"normalEval":           sample.NormalEval,
+		"socialEval":           sample.SocialEval,
+		"exhaustedEval":        sample.ExhaustedEval,
+		"tiredEval":            sample.TiredEval,
+		"happyEval":            sample.HappyEval,
+		"comments":             sample.Comments,
+	}
+
+	pt, err := client.NewPoint("emotionEvaluation", nil, fields, stringToDate(sample.TimeEndFillingForm))
+	if err != nil {
+		log.Fatal(err)
+	}
+	bp.AddPoint(pt)
+
+	// Write the batch
+	if err := c.Write(bp); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("added point!")
+
+	// Close client resources
+	if err := c.Close(); err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -127,11 +255,24 @@ func (s *server) typerSentFieldCall() http.HandlerFunc {
 	}
 }
 
+func LoadConfiguration(file string) Config {
+	var config Config
+	configFile, err := os.Open(file)
+	defer configFile.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(&config)
+	return config
+}
+
 func main() {
+	config := LoadConfiguration("../config/server_config.json")
 	s := server{
 		router: http.NewServeMux(),
 	}
-	s.routes()
+	s.routes(config.DatabaseConfig)
 	log.Fatal(http.ListenAndServe(":9000", s.router))
 
 }
